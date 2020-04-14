b03ca71f1218617fe3d99d2182f8f67f49d55cc8
Joseph Liu <josephliu@Josephs-MacBook-Air.local>
2020-04-13 22:33:56 -0400

    added files

---
+++ CompoundInterest.py
@@ -0,0 +1,5 @@
+Money = float(input())
+Year1 = Money * 1.04
+Year2 = Year1 * 1.04
+Year3 = Year2 * 1.04
+print("In the first year, you will have", round(Year1, 2) , ". In the second year you will have" , round(Year2, 2) , ". In the third year you will have" , round(Year3,2) ".")
---
+++ VolumeOfCylinder.py
@@ -0,0 +1,5 @@
+from math import pi
+Height0 = float(input())
+Radius0 = float(input())
+Volume = (pi * (Radius0 * Radius0) * Height0)
+print(round(Volume , 1))
